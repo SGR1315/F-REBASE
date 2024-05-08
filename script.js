744b0ee02a2c28f4e7dac7f458e8d546ac7fcf49
SGR1315 <146672773+SGR1315@users.noreply.github.com>
2024-05-09 00:17:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBdKiU4LbWOSJPyPHm9iFdM4LXZjmCkL-0",
-  authDomain: "jsbolgedeneme-5a05c.firebaseapp.com",
-  projectId: "jsbolgedeneme-5a05c",
-  storageBucket: "jsbolgedeneme-5a05c.appspot.com",
-  messagingSenderId: "641904683110",
-  appId: "1:641904683110:web:1a6e173a96f816b3aa25a7",
-  measurementId: "G-4R4FT3D2V7"
+  apiKey: "AIzaSyD5vhZ-zRKyb6yoijWHBSe2LtmmSAhgLeE",
+  authDomain: "bolgeler-c720a.firebaseapp.com",
+  databaseURL: "https://bolgeler-c720a-default-rtdb.firebaseio.com",
+  projectId: "bolgeler-c720a",
+  storageBucket: "bolgeler-c720a.appspot.com",
+  messagingSenderId: "1060512122848",
+  appId: "1:1060512122848:web:605a36c542eb10fda18d85",
+  measurementId: "G-08M3T36F66"
 };
 
 // Initialize Firebase
